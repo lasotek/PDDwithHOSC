c544f70a8edde1cb286dbce08faf5620163daf8e
lasotek <lasotek@gmail.com>
2021-01-17 20:25:38 +0100

    2021-01-17 20:25

--- _New_version/HOSCAnalyzer/HOSCs/KIIndexCol.cpp
+++ _New_version/HOSCAnalyzer/HOSCs/KIIndexCol.cpp
@@ -2,10 +2,36 @@
 #include <exception>
 namespace HOSC
 {
-    std::shared_ptr<SingleHOSC> KIIndexCol::insert(std::shared_ptr<SingleHOSC> &HOSC) 
+    KIIndexCol::KIIndexCol(int _no_nodes, func function)
+    {
+        switch (function)
+        {
+        case com_denomnator:
+        {
+            auto HOSC = std::make_shared<SingleHOSC>(_no_nodes);
+            if (HOSC->is_valid())
+                insert(HOSC);
+        }
+        break;
+
+        default:
+            for (int i = 0; i < _no_nodes; i++)
+            {
+                for (int j = i + 1; j < _no_nodes; j++)
+                {
+                    auto HOSC = std::make_shared<SingleHOSC>(j, i, _no_nodes);
+                    if (HOSC->is_valid())
+                        insert(HOSC);
+                }
+            }
+            break;
+        }
+    }
+
+    std::shared_ptr<SingleHOSC> KIIndexCol::insert(std::shared_ptr<SingleHOSC> &HOSC)
     {
         auto uniqueHOSC = _HOSCmap.insert_unique(HOSC);
-        if(uniqueHOSC != HOSC)
+        if (uniqueHOSC != HOSC)
         {
             uniqueHOSC->combine(*HOSC);
         }
--- _New_version/HOSCAnalyzer/HOSCs/KIIndexCol.h
+++ _New_version/HOSCAnalyzer/HOSCs/KIIndexCol.h
@@ -15,11 +15,16 @@ namespace HOSC
     private:
         using unique_simplyHOSC_map = HOSCUniqueCollection<SingleHOSC>;
         unique_simplyHOSC_map _HOSCmap;
-        // counter_maps _counters;
     public:
-        KIIndexCol(/* args */)
-        {
-        }
+        using func = enum{com_denomnator, sum_numerators};
+        KIIndexCol() = delete;
+        /**
+         * @brief Construct a new KIIndexCol object
+         * 
+         * @param _no_nodes number of nodes in graph
+         * @param function type of set: com_denominator, sum_numerators
+         */
+        KIIndexCol(int _no_nodes, func function = com_denomnator);
         ~KIIndexCol() {}
         std::shared_ptr<SingleHOSC> insert(std::shared_ptr<SingleHOSC> &HOSC);
     };
